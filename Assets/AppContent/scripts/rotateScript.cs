df112a2c452d0fbf93bc628d14c340acc3bcf049
bzbomer <omerbozaba@gmail.com>
2020-01-29 16:36:08 +0300

    First Commit

---
+++ Assets/AppContent/scripts/rotateScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateScript : MonoBehaviour
+{
+
+    public float x, y, z;
+    public float scalex, scaley, scalez;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.Rotate(x, y, z, Space.Self);
+        transform.localScale = new Vector3(scalex, scaley, scalez);
+
+    }
+}
